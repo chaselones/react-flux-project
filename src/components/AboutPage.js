ac7e8d0f7ca41c4ec4ec4ae27a867053b82f72b7
Chase Lones <chaselones@gmail.com>
2021-04-07 19:35:28 -0700

    MO REACT BABY
    MOOOOO REACT

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      // React fragment below
+      <>
+        {/* using the react fragment will return only the H2 element */}
+        <h2>About</h2>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HomePage from "./components/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import AboutPage from "./components/AboutPage";
+
+ReactDOM.render(<HomePage />, document.querySelector("#root"));
